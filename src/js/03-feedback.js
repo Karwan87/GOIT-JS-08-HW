a883b8d9e87adb09c256d64bf0e5190971fee1ef
Karwan87 <damianuszynski@gmail.com>
2023-03-05 15:12:01 +0100

    Zadanie 3 - formularz kontaktowy

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
 
+feedbackForm.addEventListener('input', () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+});
+const savedFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (savedFormState) {
+  emailInput.value = savedFormState.email;
+  messageInput.value = savedFormState.message;
+}
+feedbackForm.addEventListener('submit', event => {
+  console.log('Form data:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  event.preventDefault();
+});
