cef08cfaae9ef102bb559455e9f053bcb5aa0e14
Karwan87 <damianuszynski@gmail.com>
2023-03-05 14:48:26 +0100

    Zadanie 2 - odtwarzacz video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Vimeo from '@vimeo/player';
 
+const player = new Vimeo('vimeo-player');
+
+player.on('timeupdate', async function (data) {
+  const duration = await player.getDuration();
+  const seconds = data.seconds;
+  const percent = (seconds / duration) * 100;
+
+  console.log(
+    `Duration: ${formatTime(duration)}, Seconds: ${formatTime(
+      seconds
+    )}, Percent: ${percent.toFixed(2)}%`
+  );
+
+  localStorage.setItem('videoplayer-current-time', seconds);
+});
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
